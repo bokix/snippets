cd286aa8e3950832965c4d138f230c7debbfe4c1
bokix <bokix@163.com>
2016-07-28 11:32:10 +0800

    update

--- python/print-folder-tree/print-folder-tree.py
+++ python/print-folder-tree/print-folder-tree.py
@@ -1,5 +1,6 @@
+"""
+print folder tree.
 
-'''
 ├─cdr
 │  ├─cdr_audit
 │  ├─ts_file
@@ -12,29 +13,32 @@
 │  │  ├─3rd_md5
 │  │  ├─3rd_xml
 
-'''
+"""
+#!/usr/local/bin/py
 
 import os
-def makeResult(fileName):
-	if os.isdir(fileName):
-		pass
+tapChar="─"
+tap=4
+def myPrint(fileName,tapSize=0):
+	if tapSize==0:
+		print(os.path.split(fileName)[1])
+		return
+	print("├{}{}".format(tapChar*(tapSize-tap), os.path.split(fileName)[1]))
 
-def getContent(pathName):
+def getContent(pathName,tapSize=0):
 	#print("path name {}".format(pathName))
 	if os.path.isdir(pathName):
 		pathContent = os.listdir(pathName)
 		pathContent = [x for x in pathContent if not x.startswith((".","~"))]
-		#print("content of path:{}".format(pathContent))
 		for ele in pathContent:
-			getContent(os.path.join(pathName,ele))
-	else:
-		print(os.path.basename(pathName))
+			myPrint(os.path.join(pathName,ele),tapSize)
+			getContent(os.path.join(pathName,ele), tapSize+tap)
 
-os.chdir("XC")
+targetFolder="/Users/bokix/Desktop/"
+#os.chdir(targetFolder)
+myPrint(targetFolder)
+getContent(targetFolder,tap)
 
-getContent(os.getcwd())
-
-手上
 
 
 
